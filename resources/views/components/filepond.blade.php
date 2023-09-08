051a1231e48fd94f13c4c759a3b00f51c37a0419
mcln <cristobal.lobos@gmail.com>
2023-09-08 18:04:56 -0300

    1era semana de septiembre

--- resources/views/components/filepond.blade.php
+++ resources/views/components/filepond.blade.php
@@ -1,3 +1,20 @@
-<div>
-    <!-- An unexamined life is not worth living. - Socrates -->
+<div wire:ignore x-data x-init="() => {
+    const post = FilePond.create($refs.input);
+    post.setOptions({
+        allowMultiple: {{ $attributes->has('multiple') ? 'true' : 'false' }},
+        server: {
+            process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
+                @this.upload('{{ $attributes->whereStartsWith('wire:model')->first() }}', file, load, error, progress)
+            },
+            revert: (filename, load) => {
+                @this.removeUpload('{{ $attributes->whereStartsWith('wire:model')->first() }}', filename, load)
+            },
+            allowImagePreview: {{ $attributes->has('allowFileTypeValidation') ? 'true' : 'false' }},
+            imagePreviewMaxHeight: {{ $attributes->has('imagePreviewMaxHeight') ? $attributes->get('imagePreviewMaxHeight') : '256' }},
+            acceptedFileTypes: {!! $attributes->get('acceptedFileTypes') ?? 'null' !!},
+        }
+    });
+}">
+    <input type="file" x-ref="input" name="file"  />
+
 </div>
